f1fd3c5f6368936caeb176f4ff717b483d9d5e56
raphelemmanuvel <>
2023-12-16 13:58:49 -0500

    Updated scrapy settings to override request heaaders

--- web_crawler/settings.py
+++ web_crawler/settings.py
@@ -37,10 +37,11 @@ ROBOTSTXT_OBEY = True
 # TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-# }
+DEFAULT_REQUEST_HEADERS = {
+    "Authorization": "undefined",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
